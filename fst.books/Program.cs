353fc3c48ee2b2253a58fff5f6e3887291e60154
Brian Boland <Brian.Boland@trafficmanagement.com>
2017-02-15 16:35:32 -0800

    adds fst.books

---
+++ fst.books/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fst.books
+{
+
+    /// <summary>
+    /// Objective: fetch and display all of J.K. Rowling's books using google books api
+    /// google books documentation - https://developers.google.com/books/docs/v1/using#WorkingVolumes 
+    /// 
+    /// Instructions:
+    /// Create a C# Class to Convert the json into
+    /// Send a request to google books api search for J.K. Rowling as the author
+    /// print out to the console each book's:
+    ///     -title
+    ///     -number of pages
+    ///     -if it is epublished
+    ///     -its average rating 
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
